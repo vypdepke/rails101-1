833b81689c21ed276bc7f51c4171db85aedaa044
vypdepke <vypdepke@gmail.com>
2019-05-12 01:21:43 +0800

    implement posts pagination with will_paginate

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,7 +9,7 @@ before_action :find_group_and_check_permission, only: [:eidt, :update, :destroy]
   #各討論版專屬介面
   def show
     @group = Group.find(params[:id])
-    @posts = @group.posts.recent
+    @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)
   end
   #新增討論區用的'新增頁面'
   def new
