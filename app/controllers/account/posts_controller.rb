f620300c690ae2adade5ef975bd6019e65b490e9
vypdepke <vypdepke@gmail.com>
2019-05-12 10:59:03 +0800

    implement edit/delete btn function in my posts

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -3,4 +3,27 @@ class Account::PostsController < ApplicationController
   def index
     @posts = current_user.posts
   end
+
+  def update
+    @post = current_user.posts.find(params[:id])
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def edit
+    @post = current_user.posts.find(params[:id])
+  end
+
+  def destroy
+    @post = current_user.posts.find(params[:id])
+    @post.destroy
+    redirect_to account_posts_path
+  end
+  private
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
